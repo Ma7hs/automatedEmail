48dca135aa50a1b98eaf3e09db9ddeeb20cfd2eb
Ma7hs <Matheus.siqueira100@gmail.com>
2022-12-07 09:45:24 -0300

    commit

--- automatedEmail.py
+++ automatedEmail.py
@@ -1,31 +1,8 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[5]:
-
-
-get_ipython().system('pip install yfinance')
-
-
-# In[7]:
-
-
-get_ipython().system('pip install pyautogui')
-
-
-# In[9]:
-
-
-get_ipython().system('pip install pyperClip')
-
-
-# In[ ]:
-
-
 import yfinance
 import pyautogui
 import pyperclip
 import time
+import datetime
 
 def waitingTime(time):
     time.sleep(time)
@@ -40,44 +17,41 @@ gettingClose = dateTicker.Close
 maxClose = gettingClose.max()
 minClose = gettingClose.min()
 currentClose = gettingClose[-1]
+today = datetime.date.today()
 
 time.sleep(3)
 pyautogui.hotkey("ctrl","t")
 pyperclip.copy("www.gmail.com")
 pyautogui.hotkey("ctrl","v")
 pyautogui.hotkey("enter")
-
-time.sleep(20)
+time.sleep(10)
 pyautogui.click(x=72, y=201)
-
-pyautogui.copy(getEmail)
+timE.sleep(5)
+pyperclip.copy(getEmail)
 pyautogui.hotkey("ctrl","v")
 pyautogui.hotkey("tab")
-
-pyautogui.copy(f"Análise das ações diárias da empresa {getTicker}")
-pyautogui.hotkey("ctrl","v")
-pyautogui.hotkey("tab")
-
-
-
-# In[ ]:
-
-
-import time
-import pyautogui
-
 time.sleep(5)
-pyautogui.position()
+pyperclip.copy(f"Análise das ações diárias da empresa {getTicker}")
+pyautogui.hotkey("ctrl","v")
+pyautogui.hotkey("tab")
+time.sleep(5)
+pyperclip.copy(f"""
+Bom dia {getClient}!
 
+Segue as análises da empresa {getTicker}!
 
-# In[ ]:
+Atualmente a cotação da empresa no dia {today} é de R${round(currentClose, 2)}
 
+O fechamento mínimo foi de: R${round(minClose, 2)}
 
+O fechamento máximo foi de: R${round(maxClose, 2)}
 
+Para qualquer dúvida sobre nosso sistema de análises, basta enviar um email para nosso suporte!
 
-
-# In[ ]:
-
-
+Atenciosamente,
+Matheus Siqueira!
+""")
+pyautogui.hotkey("ctrl","v")
+pyautogui.hotkey("tab")
 
 
